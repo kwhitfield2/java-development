ad009434d1faec6e677d257407f82877ee5f52d6
kwhitfield2 <karringtonwhitfield@gmail.com>
2024-10-04 15:58:40 -0500

    init commit

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+
+
+//import java.util.Scanner;
+
+public class Book {
+    public Book() {
+    }
+
+    public static void main(String[] args) {
+
+
+
+    //       Methods
+    //       String name = "";
+    //       Scanner keyboard = new Scanner(name);
+    //       System.out.println(keyboard.next("Checked Out to: "));
+
+    //        Constructors
+
+        private String isbn, title, checkedOutTo;
+        private int id;
+        private boolean isCheckedOutTo;
+
+        public Book() {
+            this.id = 0;
+            this.isbn = "";
+            this.title = "";
+            this.isCheckedOutTo = true;
+            this.checkedOutTo = "";
+
+        }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+        }
+
+
+
+
+
+
